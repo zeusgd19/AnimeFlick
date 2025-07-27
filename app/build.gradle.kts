57da49a5ccad77615505f52357d78d8cbd8777e5
zeusgd19 <dariusgd19@gmail.com>
2025-07-27 18:22:21 +0200

    New version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.zeusgd.AnimeFlick"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
