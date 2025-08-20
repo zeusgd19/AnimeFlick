660953fc2aec408bdaed61551a43856eccf7e6c2
zeusgd19 <dariusgd19@gmail.com>
2025-08-20 21:44:38 +0200

    Cambiada version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.zeusgd.AnimeFlick"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
