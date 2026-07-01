0d0a826a254f6dae6cbdd827a6605e6e651aca09
zeusgd19 <dariusgd19@gmail.com>
2026-07-01 21:02:04 +0200

    chore: bump version to 1.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.zeusgd.AnimeFlick"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.5.2"
+        versionCode = 13
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
