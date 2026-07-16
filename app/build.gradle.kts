831013cd234d08dfefd5397b257f5cebfeda8580
zeusgd19 <dariusgd19@gmail.com>
2026-07-16 15:50:49 +0200

    v1.5.4: usar endpoint /api/airing-animes con agrupacion por dia real

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.zeusgd.AnimeFlick"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.5.3"
+        versionCode = 14
+        versionName = "1.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
